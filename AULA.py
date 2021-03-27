be3d6ad43da140b6fa3587ab79e6e80d246fb3ab
Danilo2008-DL <63961886+Danilo2008-DL@users.noreply.github.com>
2021-03-26 23:07:22 -0300

    Initial commit

---
+++ AULA.py
@@ -0,0 +1,10 @@
+diadenascimento = int(input('Dia de nascimento'))
+mesdenascimento = int(input('Mes de nascimento'))
+anodenascimento = int(input('Ano de nascimento'))
+ano = int(input('Em que ano estamos?'))
+anosub = ano - anodenascimento
+print('parabéns acesso concedido!')
+print('Bom trabalho e bom dia!')
+if anodenascimento < 2008:
+    print('você é menor de idade acesso bloqueado!')
+
